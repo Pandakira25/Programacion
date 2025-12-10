695330f256a8ffc596d8b796a5a3ddf05caff8ec
polar <zoejastreb9@gmail.com>
2025-12-10 11:41:20 +0100

    10/12/25

---
+++ PruebaParcialArraysExepcionesA2122/src/com/dam/a/ej2/ControlDePerfiles.java
@@ -0,0 +1,10 @@
+package com.dam.a.ej2;
+
+public class ControlDePerfiles {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ PruebaParcialArraysExepcionesA2122/src/com/dam/a/ej2/Perfil.java
@@ -0,0 +1,21 @@
+package com.dam.a.ej2;
+
+public class Perfil {
+
+	private String nombre;
+	private String puestoActual;
+	private String ciudad [] = {"Madrid", "Barcelona","Málaga","Valladolid"};
+	private int numContactos;
+	
+	//Constructors
+	public Perfil(){}
+	
+	public Perfil(String nmbre, String puestoActual, String ciudad [], int numContactos) {
+		
+	}
+	
+	//toString
+	
+	//Method 
+	
+}
